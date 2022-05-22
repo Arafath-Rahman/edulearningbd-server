6013c6c3a88419aaeb1aa4b67df0eb3404298e5b
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2022-05-22 12:13:17 +0600

    port issue fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 0;
 
 //middleware
 app.use(cors());
